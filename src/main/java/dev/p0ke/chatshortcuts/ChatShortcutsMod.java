bfe8a7e60ecbee408abc7d438f76e90b1ee4585f
P0ke <alextodaro@rocketmail.com>
2019-11-24 17:50:31 -0800

    Update to v1.2

--- src/main/java/dev/p0ke/chatshortcuts/ChatShortcutsMod.java
+++ src/main/java/dev/p0ke/chatshortcuts/ChatShortcutsMod.java
@@ -14,7 +14,7 @@ public class ChatShortcutsMod {
 	
     public static final String MODID = "chatshortcuts";
     public static final String MOD_NAME = "ChatShortcuts";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
     
     private ShortcutHandler shortcutHandler;
     
